4284123645307f31dd19c831b849f19a24b44c26
csteinmetz1 <csteinmetz1@gmail.com>
2021-01-08 19:57:58 +0000

    adding in ability to save out processed audio during test

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import sys
 import glob
 import json
 import torch
+import pickle
+import torchaudio
 import numpy as np
 import torchsummary
 import pytorch_lightning as pl
@@ -24,12 +26,13 @@ parser = ArgumentParser()
 # add PROGRAM level args
 parser.add_argument('--root_dir', type=str, default='./data')
 parser.add_argument('--model_dir', type=str, default='./lightning_logs/bulk')
+parser.add_argument('--save_dir', type=str, default=None)
 parser.add_argument('--preload', action="store_true", default=False)
 parser.add_argument('--half', action="store_true", default=False)
 parser.add_argument('--sample_rate', type=int, default=44100)
 parser.add_argument('--eval_subset', type=str, default='val')
-parser.add_argument('--eval_length', type=int, default=131072)
-parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--eval_length', type=int, default=8388608)
+parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--num_workers', type=int, default=32)
 
 # parse them args
@@ -47,7 +50,11 @@ test_dataloader = torch.utils.data.DataLoader(test_dataset,
                                                batch_size=args.batch_size,
                                                num_workers=args.num_workers)
 
-results = {}
+overall_results = {}
+
+if args.save_dir is not None:
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
 
 # set up loss functions for evaluation
 l1   = torch.nn.L1Loss()
@@ -57,6 +64,8 @@ models = sorted(glob.glob(os.path.join(args.model_dir, "*")))
 
 for idx, model_dir in enumerate(models):
 
+    results = {}
+
     checkpoint_path = glob.glob(os.path.join(model_dir,
                                              "lightning_logs",
                                              "version_0",
@@ -64,6 +73,7 @@ for idx, model_dir in enumerate(models):
                                              "*"))[0]
     hparams_file = os.path.join(model_dir, "hparams.yaml")
 
+    model_id = os.path.basename(model_dir)
     model_type = os.path.basename(model_dir).split('-')[1]
     epoch = int(os.path.basename(checkpoint_path).split('-')[0].split('=')[-1])
 
@@ -107,10 +117,11 @@ for idx, model_dir in enumerate(models):
             target_crop = center_crop(target, output.shape)
             input_crop = center_crop(input, output.shape)
 
-        for i, o, t, p in zip(torch.split(input_crop, 1, dim=0),
-                           torch.split(output, 1, dim=0),
-                           torch.split(target_crop, 1, dim=0),
-                           torch.split(params, 1, dim=0)):
+        for idx, (i, o, t, p) in enumerate(zip(
+                                            torch.split(input_crop, 1, dim=0),
+                                            torch.split(output, 1, dim=0),
+                                            torch.split(target_crop, 1, dim=0),
+                                            torch.split(params, 1, dim=0))):
 
             l1_loss = l1(o, t).cpu().numpy()
             stft_loss = stft(o, t).cpu().numpy()
@@ -121,6 +132,17 @@ for idx, model_dir in enumerate(models):
 
             params = p.squeeze().cpu().numpy()
             params_key = f"{params[0]:1.0f}-{params[1]*100:03.0f}"
+
+            if args.save_dir is not None:
+                ofile = os.path.join(args.save_dir, f"{params_key}-{bidx}-output--{model_id}.wav")
+                ifile = os.path.join(args.save_dir, f"{params_key}-{bidx}-input.wav")
+                tfile = os.path.join(args.save_dir, f"{params_key}-{bidx}-target.wav")
+
+                torchaudio.save(ofile, o.view(1,-1).cpu().float(), 44100)
+                if not os.path.isfile(ifile):
+                    torchaudio.save(ifile, i.view(1,-1).cpu().float(), 44100)
+                if not os.path.isfile(tfile):
+                    torchaudio.save(tfile, t.view(1,-1).cpu().float(), 44100)
 
             if params_key not in list(results.keys()):
                 results[params_key] = {
@@ -138,6 +160,24 @@ for idx, model_dir in enumerate(models):
                 results[params_key]["Agg"].append(aggregate_loss)
 
     # store in dict
+    l1_scores = []
+    stft_scores = []
+    agg_scores = []
+    print("-" * 64)
+    print("Config      L1         STFT        Agg")
+    print("-" * 64)
     for key, val in results.items():
-        print(f"{key}    L1: {np.mean(val['L1']):0.2e}    STFT: {np.mean(val['STFT']):0.3f}  STFTi: {np.mean(val['STFTi']):0.3f}   Agg: {np.mean(val['Agg']):0.3f}")
+        print(f"{key}    {np.mean(val['L1']):0.2e}    {np.mean(val['STFT']):0.3f}     {np.mean(val['Agg']):0.3f}")
 
+        l1_scores += val["L1"]
+        stft_scores += val["STFT"]
+        agg_scores += val["Agg"]
+
+    print("-" * 64)
+    print(f"Mean     {np.mean(l1_scores):0.2e}    {np.mean(stft_scores):0.3f}      {np.mean(agg_scores):0.3f} ")
+    print()
+    overall_results[model_id] = results
+
+pickle.dump(overall_results, open(f"test_results_{args.eval_subset}.p", "wb" ))
+
+# we can make some kind of scatter plot to visualize this
