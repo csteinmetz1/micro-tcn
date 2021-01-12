80d4e3ce204541859a020f9f0ed6c1aaeb7ab6f5
csteinmetz1 <csteinmetz1@gmail.com>
2021-01-12 21:29:31 +0000

    adding in the ability to measure MACs in addition to RTF in speed test script

--- speed.py
+++ speed.py
@@ -4,6 +4,7 @@ import time
 import torch
 import numpy as np
 import torchsummary
+import pandas as pd
 from itertools import product
 import pytorch_lightning as pl
 from argparse import ArgumentParser
@@ -22,7 +23,7 @@ def compute_receptive_field(nblocks, dilation_growth, kernel_size, stack_size=10
         rf = rf + ((kernel_size-1) * dilation)
     return rf
 
-def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", N=44100):
+def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", causal=False, N=44100, gpu=False):
 
     pl.seed_everything(42) # set the seed
 
@@ -31,14 +32,15 @@ def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", N=44
     dict_args["nblocks"] = nblocks
     dict_args["kernel_size"] = kernel_size
     dict_args["channel_width"] = 32
+    dict_args["hidden_size"] = 32
     dict_args["grouped"] = False
+    dict_args["causal"] = causal
     dict_args["dilation_growth"] = dilation_growth
 
-    gpu = False
     sr = 44100
     #N = 44100
     duration = N/sr # seconds 
-    n_iters = 100
+    n_iters = 10
     timings = []
 
     if model_type == "TCN": 
@@ -55,7 +57,7 @@ def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", N=44
         input = (torch.rand(1,1,samples) * 2) - 1
     else:
         rf = 0
-        model = torch.jit.script(LSTMModel(**dict_args)) # create the model with args
+        model = LSTMModel(**dict_args) # create the model with args
         input = (torch.rand(1,1,N) * 2) - 1
 
     # count number of parameters
@@ -81,8 +83,8 @@ def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", N=44
             output = model(input, params)
             toc = time.perf_counter()
             timings.append(toc-tic)
-            #sys.stdout.write(f"{n+1:3d}/{n_iters:3d}\r")
-            #sys.stdout.flush()
+            sys.stdout.write(f"{n+1:3d}/{n_iters:3d}\r")
+            sys.stdout.flush()
 
     mean_time_s = np.mean(timings)
     mean_time_ms = mean_time_s * 1e3
@@ -95,11 +97,15 @@ def run(nblocks, dilation_growth, kernel_size, target_rf, model_type="TCN", N=44
 
 if __name__ == '__main__':
 
-    plot = False
-    full = False
-    target_rf = 300 # ms
-    max_dilation = 10
-    max_blocks = 12
+    parser = ArgumentParser()
+    parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--full", action="store_true")
+    parser.add_argument("--gpu", action="store_true")
+    parser.add_argument("--rf", action="store_true")
+    args = parser.parse_args()
+
+    max_dilation = 128
+    max_blocks = 5
     max_kernel = 33
 
     dilation_factors = np.arange(1,max_dilation+1)
@@ -108,11 +114,11 @@ if __name__ == '__main__':
 
     candidates = []
 
-    if full:
+    if args.full:
         for b, d, k in product(nblocks, dilation_factors, kernels):
             print(b, d, k)
-            rf, rtf = run(b, d, k, target_rf, N=2048)
-            if rf > target_rf:
+            rf, rtf = run(b, d, k, args.rf, N=512)
+            if rf > args.rf:
                 candidates.append({
                     "kernel" : k,
                     "dilation": d,
@@ -121,30 +127,51 @@ if __name__ == '__main__':
                     "rtf" : rtf
                 })
     else:
-        nblocks = [4, 4, 5, 10]
-        dilation_factors = [10, 10, 10, 2]
-        kernels = [5, 13, 5, 15]
-        for b, d, k in zip(nblocks, dilation_factors, kernels):
-            print(b, d, k)
-            rf, rtf = run(b, d, k, -1, N=2048)
-            candidates.append({
-                "kernel" : k,
-                "dilation": d,
-                "blocks" : b,
-                "rf" : rf,
-                "rtf" : rtf
-            })
+        frame_sizes = [128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
+        causal      = [True, False]
+        for c, N in product(causal, frame_sizes):
+            model_id = ["TCN-370", "TCN-100", "TCN-300", "TCN-1000", "TCN-324", "LSTM-32"]
+            model_type = ["TCN", "TCN", "TCN", "TCN", "TCN", "LSTM"]
+            nblocks          = [ 3, 4,  4,  5, 10, 0]
+            dilation_factors = [64,10, 10, 10,  2, 0]
+            kernels          = [ 5, 5, 13,  5, 15, 0]
+            for mid, m, b, d, k in zip(model_id, 
+                                        model_type, 
+                                        nblocks, 
+                                        dilation_factors, 
+                                        kernels):
+                print(b, d, k)
+                rf, rtf = run(b, d, k, -1, causal=c, N=N, model_type=m, gpu=args.gpu)
+                if c:   mid += "-C"
+                else:   mid += "-N"
+                candidates.append({
+                    "model_id": mid,
+                    "causal" : c,
+                    "kernel" : k,
+                    "dilation": d,
+                    "blocks" : b,
+                    "rf" : rf,
+                    "rtf" : rtf,
+                    "N" : N
+                })
+
+    df = pd.DataFrame(candidates)
+    print(df)
+    if args.gpu:
+        df.to_csv('speed_gpu.csv')
+    else:
+        df.to_csv('speed_cpu.csv')
 
     # find the optimal architecture
-    sorted_candidates = sorted(candidates, key = lambda x: x["rtf"], reverse=True)
+    #sorted_candidates = sorted(candidates, key = lambda x: x["rtf"], reverse=True)
     print("-"*50)
-    print("     RTF       RF      Blocks  Dilation   Kernel")
+    print("     ID      RTF       RF      Blocks  Dilation   Kernel")
     print("-"*50)
-    for n, c in enumerate(sorted_candidates[:11]):
-        print(f"{n: 3d} {c['rtf']: 2.2f}x  {c['rf']:0.1f} ms    {c['blocks']}        {c['dilation']}        {c['kernel']}")
+    for n, c in enumerate(candidates[:11]):
+        print(f"{n: 3d} {c['model_id']}  {c['rtf']: 2.2f}x  {c['rf']:0.1f} ms    {c['blocks']}        {c['dilation']}        {c['kernel']}")
     print("-"*50)
 
-    if plot:
+    if args.plot:
         fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(10,4))
 
         norm = colors.DivergingNorm(vmin=0, vcenter=1)
