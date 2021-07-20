9da87f76d34267a9d77530eeb70885ac3385e2fc
csteinmetz1 <csteinmetz1@gmail.com>
2021-07-20 08:53:05 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Inspired from https://github.com/kennethreitz/setup.py
+
+from pathlib import Path
+from setuptools import setup, find_packages
+
+NAME = "micro-tcn"
+DESCRIPTION = "Efficient neural networks for analog audio effect modeling"
+URL = "https://github.com/csteinmetz1/micro-tcn"
+EMAIL = "c.j.steinmetz@qmul.ac.uk"
+AUTHOR = "Christian Steinmetz"
+REQUIRES_PYTHON = ">=3.8.0"
+VERSION = "0.1.0"
+
+HERE = Path(__file__).parent
+
+try:
+    with open(HERE / "README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=["microtcn"],
+    install_requires=["torch", "scipy", "numpy", "librosa", "matplotlib"],
+    extras_require={"test": ["resampy"]},
+    include_package_data=True,
+    license="Apache License 2.0",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+    ],
+)
